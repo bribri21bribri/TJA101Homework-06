ef87146d5db125a63cc2d18b9229deabe9117c8c
brian.chao <kuroiwaruiko21@gmail.com>
2025-04-13 14:49:53 +0800

    homework6

---
+++ src/hw6/CalTest.java
@@ -0,0 +1,20 @@
+package hw6;
+
+import java.util.Scanner;
+
+public class CalTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("請輸入x的值");
+        int x = sc.nextInt();
+        System.out.println("請輸入y的值");
+        int y = sc.nextInt();
+
+        try {
+            int result = Calculator.powerXY(x, y);
+            System.out.println(x + "的" + y + "次方等於" + result);
+        } catch (CalException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
